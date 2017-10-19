35f1ff059b18c16ca904677e2a2bd673a00587ac
Blanca Sanchez <blancayllen@gmail.com>
2017-10-19 01:45:26 -0400

    navbar functionality

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default class Navbar extends Component {
 
   render() {
     return (
-      <nav>
+      <div>
         <h3>Your Navbar</h3>
-      </nav>
+        <button>
+          <NavLink to={'/campus'}>
+            Campuses
+          </NavLink>
+        </button>
+        <button>
+          <NavLink to={'/student'}>
+            Students
+          </NavLink>
+        </button>
+      </div>
     )
   }
 
